6941fe2fb765d7f2b4e8c22811f873177b278b99
Cracky Boy <thibault.varga@student.heaj.be>
2025-09-06 19:14:00 +0200

    Mise en place des styles colors

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,20 @@
 import { ThemedText } from "@/components/ThemedText";
+import { useThemeColors } from "@/hooks/useThemeColors";
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const colors = useThemeColors()
   return (
-    <SafeAreaView style={styles.container}>
-      <ThemedText variant="headline">Pokédex</ThemedText>
+    <SafeAreaView style={[styles.container, {backgroundColor: colors.tint}]}>
+      <ThemedText variant="headline" color="grayWhite">Pokédex</ThemedText>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {backgroundColor: '#ff0000'}
+  container: {
+    flex: 1,
+  }
 })
--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,6 +1,8 @@
+import { Colors } from "@/constants/Colors";
+import { useThemeColors } from "@/hooks/useThemeColors";
 import { StyleSheet, Text, type TextProps } from "react-native";
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
     body3: {
         fontSize: 10,
         lineHeight: 16,
@@ -33,9 +35,10 @@ const styles = StyleSheet.create({
 
 type Props = TextProps & { 
     variant?: keyof typeof styles,
-    color?: string,
+    color?: keyof typeof Colors["light"],
 }
 
 export function ThemedText ({variant, color, ...rest}: Props) {
-    return <Text style={styles[variant ?? 'body3']} {...rest} />
+    const colors = useThemeColors()
+    return <Text style={[styles[variant ?? 'body3'], {color: colors[color ?? "grayDark"]}]} {...rest} />
 }
